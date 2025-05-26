83088909a442d2118909f9035988f5015c8ea11c
tf63 <74246282+tf63@users.noreply.github.com>
2025-05-26 21:05:21 +0900

    feat: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <main className="h-dvh">
+            <h1>
+                <span className="text-4xl font-bold">Welcome to</span>
+                <br />
+                <span className="text-6xl text-blue-500">Next.js with Tailwind CSS!</span>
+            </h1>
+        </main>
+    )
+}
